e174ff13bd4b4810b9e990dae9f27fa8526c0b3c
akashelangovan <akash.elangovan07@gmail.com>
2022-07-15 14:25:03 +0530

    Added a line

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -8,6 +8,7 @@ namespace JenkinsApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins");
+            Console.WriteLine("Press a button to end the application");
             Console.ReadLine();
         }
     }
